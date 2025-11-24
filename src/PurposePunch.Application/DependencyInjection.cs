da999be32c71bf178e906e6f63b2440a7e243287
Donkrzawayan <krzysztofdoniec8+github@gmail.com>
2025-11-24 21:29:57 +0100

    basic FluentValidation

--- src/PurposePunch.Application/DependencyInjection.cs
+++ src/PurposePunch.Application/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using PurposePunch.Application.Common.Behaviors;
 
 namespace PurposePunch.Application;
 
@@ -6,7 +9,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(assembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        });
+
+        services.AddValidatorsFromAssembly(assembly);
 
         return services;
     }
--- src/PurposePunch.Application/Features/Decisions/CreateDecision.cs
+++ src/PurposePunch.Application/Features/Decisions/CreateDecision.cs
@@ -1,10 +1,24 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using PurposePunch.Application.Interfaces;
 using PurposePunch.Domain.Entities;
 
 namespace PurposePunch.Application.Features.Decisions;
 
 public record CreateDecisionCommand(string Title, string Description) : IRequest<Decision>;
+
+public class CreateDecisionValidator : AbstractValidator<CreateDecisionCommand>
+{
+    public CreateDecisionValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(60).WithMessage("The title cannot be longer than 60 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.");
+    }
+}
 
 public class CreateDecisionHandler : IRequestHandler<CreateDecisionCommand, Decision>
 {
